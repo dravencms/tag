3089f1215e4678c8ccb19b78b7d90e2c594d9595
Adam Schubert <adam.schubert@sg1-game.net>
2016-12-09 01:47:30 +0100

    Fixtures

---
+++ src/Dravencms/Model/Tag/Fixtures/AdminMenuFixtures.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Copyright (C) 2016 Adam Schubert <adam.schubert@sg1-game.net>.
+ */
+
+namespace Dravencms\Model\Tag\Fixtures;
+
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Dravencms\Model\Admin\Entities\Menu;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class AdminMenuFixtures extends AbstractFixture implements DependentFixtureInterface
+{
+    /**
+     * Load data fixtures with the passed EntityManager
+     *
+     * @param ObjectManager $manager
+     */
+    public function load(ObjectManager $manager)
+    {
+        $child = new Menu('Tags', ':Admin:Tag:Tag', 'fa-tags', $this->getReference('user-acl-operation-tag-edit'));
+        $manager->persist($child);
+    }
+    /**
+     * Get the order of this fixture
+     *
+     * @return integer
+     */
+    public function getDependencies()
+    {
+        return ['Dravencms\Model\Tag\Fixtures\AclOperationFixtures'];
+    }
+}
